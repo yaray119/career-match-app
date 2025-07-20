34313f4862a7b07162ea3b21242b1d10a77e496a
yaray119 <ygyingli@ncsu.edu>
2025-07-20 17:26:26 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -76,6 +76,3 @@ if user_skills:
         st.markdown("---")
 
 
-This is your README. READMEs are where you can communicate what your project is and how to use it.
-
-Write your name on line 6, save it, and then head back to GitHub Desktop.
